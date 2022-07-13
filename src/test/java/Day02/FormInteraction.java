1a7d0bf7fd43ea56c9f8c315c68ac161dc7b398e
Thi Tran <thitran@iMac.local>
2022-07-13 16:57:50 +0700

    update code day02|done

--- src/test/java/Day02/FormInteraction.java
+++ src/test/java/Day02/FormInteraction.java
@@ -3,12 +3,34 @@ package Day02;
 import driver.DriverFactory;
 import driver.Platforms;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
-import javafx.application.Platform;
+
 
 public class FormInteraction {
-    public static void main(String[] args) {
-        //AppiumDriver<MobileElement> appiumDriver = DriverFactory.getDriver(Platforms.android)
 
+    public static void main(String[] args) {
+        AppiumDriver<MobileElement> appiumDriver = DriverFactory.getDriver(Platforms.android);
+
+        try {
+            //Click Form button
+            MobileElement formBtnElem = appiumDriver.findElement(MobileBy.AccessibilityId("Forms"));
+            formBtnElem.click();
+            Thread.sleep(3000);
+            //Input fields
+            MobileElement fieldInputElem = appiumDriver.findElement(MobileBy.AccessibilityId("text-input"));
+            fieldInputElem.sendKeys("Mai Thi nè");
+            Thread.sleep(3000);
+            //Switch
+            MobileElement switchBtnElem = appiumDriver.findElement(MobileBy.AccessibilityId("switch"));
+            switchBtnElem.click();
+            Thread.sleep(3000);
+            //Click dropdown
+            MobileElement dropdownSelectElem = appiumDriver.findElement(MobileBy.xpath("//android.view.ViewGroup[@content-desc=\"Dropdown\"]/android.view.ViewGroup/android.widget.EditText"));
+            dropdownSelectElem.click();
+            Thread.sleep(3000);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
